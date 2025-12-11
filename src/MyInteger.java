41f1d1527ba123c1727b9280b89f33c818f191f9
Parker0000000 <flowers.par8470@stu.stech.edu>
2025-12-11 10:13:13 -0700

    added MyInteger

---
+++ src/MyInteger.java
@@ -0,0 +1,21 @@
+public class MyInteger {
+    double newint;
+    boolean even;
+MyInteger(int MyInt){
+    newint = MyInt;
+}
+
+int isEven() {
+if newint % 2 == 0;{
+    return 0;
+} else {
+    return 1;
+}
+}
+int isOdd() {
+
+}
+int isPrime() {
+    
+}
+}
---
+++ src/MyIntigerTest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MyIntigerTest {
+    public static void main(String[] args) throws Exception {
+        int MyInt = 0;
+
+        Scanner input = new Scanner(System.in);
+        MyInt = 14 % 2;
+        System.out.println(MyInt);
+        MyInteger integer = new MyInteger(MyInt);
+    }
+}
