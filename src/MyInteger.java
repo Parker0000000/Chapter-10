c5ab649aa790fd3ad03d89922b3e3d8ac869394d
Parker0000000 <flowers.par8470@stu.stech.edu>
2026-01-27 10:19:55 -0700

    added the static methods

--- src/MyInteger.java
+++ src/MyInteger.java
@@ -1,41 +1,78 @@
 public class MyInteger {
      int newint;
-
+MyInteger vlaue = 0;
 MyInteger(int MyInt){
     newint = MyInt;
+    
 }
-
+//Normal code
 String isEven() {
-    if (newint % 2 == 0){
-        return "True";
-    }  else {
-        return "False";
-    }
+    
+    return isEven(newint);
 }
 
 String isOdd() {
-    if (newint % 2 != 0){
-        return "True";
-    }  else {
-        return "False";
-    }
+    return isOdd(newint);
 }
 
 
 String isPrime() {
+   return isPrime(newint);
+}
+
+
+
+
+//static ints
+static String isEven(int newint) {
+    if (newint % 2 == 0){
+        return "The integer is even";
+    }  else {
+        return "The integer is not even";
+    }
+}
+
+static String isOdd(int newint) {
+    if (newint % 2 != 0){
+        return "The integer is odd";
+    }  else {
+        return "The integer is not odd";
+    }
+}
+static String isPrime(int newint) {
     if (newint <= 1) {
-        return "False";
+        return "The integer is not prime";
     }
  
     for (int i = 2; i <= Math.sqrt(newint); i++) {
         if (newint % i == 0) {
-            return "False";
+            return "The integer is not prime";
         } else {
-            return "True";
+            return "The integer is prime";
         }
     }
     return "";
 }
 
+//MyInteger clauses
+static String isEven(MyInteger vlaue) {
+    
+    return isEven(vlaue.value);
+}
 
+static String isOdd(MyInteger vlaue) {
+    return isOdd(vlaue.value);
+}
+
+static String isPrime(MyInteger vlaue) {
+    return isPrime(vlaue.value);
+}
+
+//Equals code
+String equals(MyInteger vlaue) {
+    if (newint == vlaue.value) {
+        return "the integer is equal";
+    }
+    return "";
+}
 }
--- src/MyIntigerTest.java
+++ src/MyIntigerTest.java
@@ -3,17 +3,14 @@ import java.util.Scanner;
 public class MyIntigerTest {
     public static void main(String[] args) throws Exception {
         int MyInt;
-
+        
         Scanner input = new Scanner(System.in);
         MyInt = input.nextInt();
-        System.out.println(MyInt);
         MyInteger integer = new MyInteger(MyInt);
-        System.out.print("Is " + MyInt + " even: ");
         System.out.println(integer.isEven());
-        System.out.print("Is " + MyInt + " odd: ");
         System.out.println(integer.isOdd());
-        System.out.print("Is " + MyInt + " prime: ");
         System.out.println(integer.isPrime());
+        System.out.println(integer.equals(MyInt));
         
         
     }
